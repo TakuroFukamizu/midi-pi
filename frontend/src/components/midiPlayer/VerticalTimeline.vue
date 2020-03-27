7a4afb2073c39e0d80f99d7ecb7c275e4f5473e0
derushio <derushio@gmail.com>
2020-03-28 06:53:44 +0900

    <feature/frontend>[modified] 正常棒

--- frontend/src/components/midiPlayer/VerticalTimeline.vue
+++ frontend/src/components/midiPlayer/VerticalTimeline.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 k-stage.vertical-timeline(:config='config' v-resize='resized')
     k-layer
-        k-circle(v-for='circle, i in circles' :config='circle' :key='i')
+        k-rect(v-for='noteObject, i in noteObjects' :config='noteObject' :key='i')
 </template>
 
 <script lang="ts">
@@ -14,6 +14,7 @@ import gsap from 'gsap';
 import GradientColor from 'gradient-color';
 
 import RandomUtil from '@/utils/RandomUtil';
+import ArrayUtil from '../../utils/ArrayUtil';
 
 @Component({
     directives: {
@@ -28,6 +29,7 @@ export default class VerticalTimeline extends Vue {
 
     protected colors = GradientColor(['#E5404C', '#FD743C', '#FD9C3C'], 128);
 
+    protected currentTime = 0;
     protected get center(): {
         x: number;
         y: number;
@@ -38,45 +40,72 @@ export default class VerticalTimeline extends Vue {
         };
     }
 
-    protected circles = [] as Array<
-        Konva.CircleConfig & {
-            percent: number;
-        }
-    >;
+    protected noteObjects = [] as Array<Konva.RectConfig>;
+
+    protected notes = [] as Note[];
 
     protected mounted(): void {
-        setInterval(() => {
+        for (const i of ArrayUtil.range(100)) {
+            this.notes.push({
+                time: i * RandomUtil.rand(2000, 3000),
+                duration: RandomUtil.rand(100, 200),
+                value: RandomUtil.rand(0, 127),
+            });
+        }
+
+        const timeline = gsap.timeline();
+        for (const note of this.notes) {
             // eslint-disable-next-line @typescript-eslint/no-this-alias
             const self = this;
-            const circle = {
-                radius: 20,
+            const object = {
+                width: 20,
+                height: note.duration,
+                offsetX: 10,
+                offsetY: note.duration,
                 fill: this.colors[RandomUtil.rand(this.colors.length)],
-                percent: 0,
+                percent: -1,
                 get x(): number {
                     return self.config.width / 2;
                 },
                 get y(): number {
+                    if (this.percent < 0) {
+                        // 本当は非表示にしたい
+                        return -9999;
+                    }
+
                     if (100 <= this.percent) {
-                        const index = self.circles.indexOf(this);
+                        const index = self.noteObjects.indexOf(this);
                         if (index !== -1) {
-                            self.circles.splice(self.circles.indexOf(this), 1);
+                            self.noteObjects.splice(index, 1);
                         }
                     }
-                    return self.config.height * (this.percent / 100);
+
+                    return (self.config.height - 32) * (this.percent / 100);
                 },
-            };
-            this.circles.push(circle);
-            gsap.to(circle, 3, { percent: 100, ease: 'none' });
-        }, 100);
+            } as Konva.RectConfig;
+            this.noteObjects.push(object);
+
+            timeline.fromTo(
+                object,
+                5,
+                {
+                    percent: -1,
+                    ease: 'none',
+                },
+                {
+                    percent: 100,
+                    ease: 'none',
+                },
+                note.time / 1000 - 5,
+            );
+        }
+
+        timeline.seek(-2);
+        timeline.play();
     }
 
     protected resized(): void {
         this.updateConfig();
-
-        for (const circle of this.circles) {
-            circle.x = this.center.x;
-            circle.y = this.config.height * circle.percent;
-        }
     }
 
     protected updateConfig(): void {
@@ -91,6 +120,12 @@ export default class VerticalTimeline extends Vue {
     }
 }
 Vue.component('VerticalTimeline', VerticalTimeline);
+
+export interface Note {
+    time: number;
+    duration: number;
+    value: number;
+}
 </script>
 
 <style lang="stylus" scoped>
