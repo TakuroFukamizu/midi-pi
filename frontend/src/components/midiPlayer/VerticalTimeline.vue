e5b353c44ff4ca86043f25c44c754531f0c99254
derushio <derushio@gmail.com>
2020-03-28 03:33:37 +0900

    <feature/frontend>[modified] 円を表示

--- frontend/src/components/midiPlayer/VerticalTimeline.vue
+++ frontend/src/components/midiPlayer/VerticalTimeline.vue
@@ -1,10 +1,14 @@
 <template lang="pug">
-k-stage.vertical-timeline(:config='config' v-resize='updateConfig')
+k-stage.vertical-timeline(:config='config' v-resize='resized')
+    k-layer
+        k-circle(:config='circles[0]')
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import VueResizeDirective from 'vue-resize-directive';
+
+import Konva from 'konva';
 
 @Component({
     directives: {
@@ -16,6 +20,31 @@ export default class VerticalTimeline extends Vue {
         width: 0,
         height: 0,
     };
+
+    protected get center(): {
+        x: number;
+        y: number;
+    } {
+        return {
+            x: this.config.width / 2,
+            y: this.config.height / 2,
+        };
+    }
+
+    protected circles = [] as Konva.CircleConfig[];
+
+    protected resized(): void {
+        this.updateConfig();
+
+        this.circles = [
+            {
+                radius: 100,
+                fill: 'black',
+                x: this.center.x,
+                y: this.center.y,
+            },
+        ];
+    }
 
     protected updateConfig(): void {
         if (this.$el == null) {
